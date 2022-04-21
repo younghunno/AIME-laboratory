fce7635d0a0b89f1b13f178f7eaba569522309f6
Younghun Noh <74542482+younghunno@users.noreply.github.com>
2022-04-21 22:00:36 +0900

    Add files via upload

---
+++ 농구 궤적 방정식/CamRotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    float mx;
+    float my;
+
+    public Animator cubeAnim;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // 마우스의 입력을 받아오고 싶다.
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        // 마우스 입력 값을 저장 해두고 싶다. 
+        mx += mouseX;
+        my += mouseY;
+
+        // 마우스 위 아래 회전에 제한을 두고 싶다. 
+        my = Mathf.Clamp(my, -70, 70);
+
+        // 카메라의 회전 값을 저장한 값으로 넣어 주고 싶다.  
+        transform.rotation = Quaternion.Euler(new Vector3(-my, mx, 0));
+    }
+}
---
+++ 농구 궤적 방정식/StoneGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Basketball_3DS; //GameObject 선언
+    public GameObject ShootRay;
+    public Transform spawnerPosition;
+    public GameObject target;
+    //public Rigidbody target;
+
+    // Update is called once per frame
+
+
+    void Start()
+    {
+        //target = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (GameObject.Find("Main Camera").GetComponent<ShootRay>().ballcount == 1)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {   //stone을 생성하고 발사!
+                //transform.position = new Vector3(10f, 10f, -10f);
+                //Instantiate(Basketball_3DS, spawnerPosition.position);
+
+                Instantiate(target, spawnerPosition.position, spawnerPosition.rotation);
+
+                
+                //target.AddForce(transform.forward * 10);
+
+                //Basketball_3DS.GetComponent<StoneController>().Shoot(new Vector3(0, 2000, 20));
+                GameObject.Find("Main Camera").GetComponent<ShootRay>().ballcount -= 1;
+               // Debug.Log(GameObject.Find("Main Camera").GetComponent<ShootRay>().ballcount);
+            }
+        }
+    }
+}
